aa8d6348c02ee57a7eedc56cb71b1cb8f4ea4609
norahm224 <norah.213113778@gmail.com>
2022-12-15 09:57:19 +0300

    Initial Commit

---
+++ test2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  test2
+//
+//  Created by Nourah Almusaad on 15/12/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test2/test2App.swift
@@ -0,0 +1,17 @@
+//
+//  test2App.swift
+//  test2
+//
+//  Created by Nourah Almusaad on 15/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct test2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
